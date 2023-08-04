e8732246ce14cecd492e07befe9b887c87b633cb
no_name <no_name@no_name>
2023-08-04 21:28:37 +0900

    kotlin-coroutine

--- kotlin-coroutine/src/test/kotlin/booookstore/playground/HelloCoroutineTest.kt
+++ kotlin-coroutine/src/test/kotlin/booookstore/playground/HelloCoroutineTest.kt
@@ -1,4 +1,26 @@
 package booookstore.playground
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class HelloCoroutineTest {
+
+    @Test
+    fun sayHello() {
+        var message = ""
+        runBlocking {
+            launch {
+                delay(100L)
+                message += "World. "
+            }
+            message += "Hello, "
+        }
+        message += "from coroutine."
+
+        assertEquals("Hello, World. from coroutine.", message)
+    }
+
 }
