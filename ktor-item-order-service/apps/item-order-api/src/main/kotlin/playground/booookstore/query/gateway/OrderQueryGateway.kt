cbf97f74a742b1c4b05505c2a5e2801f8eda0edc
no_name <no_name@no_name>
2023-09-23 22:43:43 +0900

    item-order-api

--- ktor-item-order-service/apps/item-order-api/src/main/kotlin/playground/booookstore/query/gateway/OrderQueryGateway.kt
+++ ktor-item-order-service/apps/item-order-api/src/main/kotlin/playground/booookstore/query/gateway/OrderQueryGateway.kt
@@ -1,6 +1,6 @@
 package playground.booookstore.query.gateway
 
-import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.toKotlinLocalDateTime
 import playground.booookstore.query.driver.RDBOrderDriver
 import playground.booookstore.query.port.OrderQueryModel
 import playground.booookstore.query.port.OrderQueryPort
@@ -9,7 +9,7 @@ import playground.booookstore.query.type.OrderId
 class OrderQueryGateway(private val rdbOrderDriver: RDBOrderDriver) : OrderQueryPort {
 
     override suspend fun find(orderId: OrderId): OrderQueryModel = rdbOrderDriver.find(orderId)?.let {
-        OrderQueryModel(id = it.id, orderDateTime = LocalDateTime.parse("2023-08-18T14:52:00.00"), shopId = "9870")
+        OrderQueryModel(id = it.id, orderDateTime = it.orderDateTime.toKotlinLocalDateTime(), shopId = "9870")
     } ?: throw Exception("order not found. search by orderId[$orderId]")
 
 }
