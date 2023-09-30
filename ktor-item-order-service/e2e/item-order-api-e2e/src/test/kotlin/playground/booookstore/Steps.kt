941067d48035fc6853d767841b298c6907f65e78
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-09-30 17:08:52 +0900

    ktor-item-order-api

--- ktor-item-order-service/e2e/item-order-api-e2e/src/test/kotlin/playground/booookstore/Steps.kt
+++ ktor-item-order-service/e2e/item-order-api-e2e/src/test/kotlin/playground/booookstore/Steps.kt
@@ -1,12 +1,32 @@
 package playground.booookstore
 
 import com.thoughtworks.gauge.Step
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.runBlocking
+import kotlin.test.assertEquals
 
 class Steps {
 
+    private val httpClient = HttpClient(CIO)
+
+    private var httpResponse: HttpResponse? = null
+
     @Step("オーダーIDが<orderId>であるオーダーを取得する")
-    fun getOrderByOrderID(orderId: String) {
-        println(orderId)
+    fun getOrderByOrderID(orderId: String) = runBlocking {
+        httpResponse = httpClient.get("http://localhost:8080/order/$orderId")
+    }
+
+    @Step("レスポンスステータスが<httpResponseStatusCode>である")
+    fun assertHttpResponseStatusCode(httpResponseStatusCode: String) {
+        assertEquals(200, httpResponse!!.status.value)
+    }
+
+    @Step("レスポンスのオーダーIDは<orderId>である")
+    fun assertRespondOrderId(orderId: String) {
+        TODO()
     }
 
 }
