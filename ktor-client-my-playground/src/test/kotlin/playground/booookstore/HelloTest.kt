6c3dc5c382f6038a45bf618e78768afac84a626d
no_name <no_name@no_name>
2023-10-08 18:11:01 +0900

    ktor-client-my-playground

--- ktor-client-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
+++ ktor-client-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
@@ -1,8 +1,23 @@
 package playground.booookstore
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class HelloTest {
+
+    lateinit var httpClient: HttpClient
+
+    @Before
+    fun setupKtorHttpClient() {
+        httpClient = HttpClient(CIO)
+    }
+
+    @Test
+    fun hello() {
+        assertEquals(1, 1)
+    }
 
 }
