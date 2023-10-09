695cb55e40c6d48042cd8dd02b567743d76117a3
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-09 13:55:15 +0900

    ktor-item-order-api

--- ktor-client-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
+++ ktor-client-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
@@ -1,23 +1,47 @@
 package playground.booookstore
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class HelloTest {
 
-    lateinit var httpClient: HttpClient
+    val httpClient = HttpClient(CIO) {
+        engine {
+            maxConnectionsCount = 3
+        }
+    }
+
+    lateinit var server: WireMockServer
 
     @Before
-    fun setupKtorHttpClient() {
-        httpClient = HttpClient(CIO)
+    fun startWiremockServer() {
+        server = WireMockServer(WireMockConfiguration.options().dynamicPort().notifier(Slf4jNotifier(true))).apply {
+            start()
+            stubFor(
+                get(anyUrl()).willReturn(aResponse().withStatus(200))
+            )
+        }
+    }
+
+    @After
+    fun shutdownWiremockServer() {
+        server.shutdownServer()
     }
 
     @Test
     fun hello() {
-        assertEquals(1, 1)
+        runBlocking {
+            httpClient.get("http://localhost:${server.port()}/v1/ping")
+        }
     }
 
 }
