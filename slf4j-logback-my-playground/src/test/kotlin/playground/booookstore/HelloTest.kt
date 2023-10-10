eefb7f0e99809125e4f76c4e8f3a6a6b1181fa2c
no_name <no_name@no_name>
2023-10-10 20:29:23 +0900

    slf4j-logback-my-playground

--- slf4j-logback-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
+++ slf4j-logback-my-playground/src/test/kotlin/playground/booookstore/HelloTest.kt
@@ -1,5 +1,7 @@
 package playground.booookstore
 
+import ch.qos.logback.classic.Level.INFO
+import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.core.util.StatusPrinter
 import org.junit.Test
@@ -16,6 +18,23 @@ class Playground {
         // print internal state
         val lc = LoggerFactory.getILoggerFactory() as LoggerContext
         StatusPrinter.print(lc)
+    }
+
+    @Test
+    fun loggerLevel() {
+        val logger = LoggerFactory.getLogger("com.foo") as Logger
+        logger.level = INFO
+
+        val barlogger = LoggerFactory.getLogger("com.foo.Bar")
+
+        logger.warn("Low fuel level.")
+        // -> 20:28:02.203 [main] WARN com.foo -- Low fuel level.
+        logger.debug("Starting search for nearest gas station.")
+        // no output
+        barlogger.info("Located nearest gas station.")
+        // -> 20:28:02.205 [main] INFO com.foo.Bar -- Located nearest gas station.
+        barlogger.debug("Exiting gas station search.")
+        // no output
     }
 
 }
