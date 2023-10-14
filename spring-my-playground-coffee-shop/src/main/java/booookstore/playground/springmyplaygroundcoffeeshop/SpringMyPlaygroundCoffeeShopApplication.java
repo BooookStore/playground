c83349289e9d8015c3966b13d7e301c6a402e0ed
no_name <no_name@no_name>
2023-10-14 19:45:14 +0900

    spring-my-playground-coffee-shop

---
+++ spring-my-playground-coffee-shop/src/main/java/booookstore/playground/springmyplaygroundcoffeeshop/SpringMyPlaygroundCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package booookstore.playground.springmyplaygroundcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyPlaygroundCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyPlaygroundCoffeeShopApplication.class, args);
+	}
+
+}
