66f7f45379ab9feff0864634a343c9dbeb405c81
no_name <no_name@no_name>
2023-10-18 21:21:48 +0900

    spring-my-playground-security-basics

--- spring-my-playground-security-basics/src/main/java/booookstore/playground/spring/security/UserManagementConfiguration.java
+++ spring-my-playground-security-basics/src/main/java/booookstore/playground/spring/security/UserManagementConfiguration.java
@@ -2,16 +2,14 @@ package booookstore.playground.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ApplicationConfig {
+public class UserManagementConfiguration {
 
     @SuppressWarnings("deprecation")
     @Bean
@@ -20,17 +18,12 @@ public class ApplicationConfig {
     }
 
     @Bean
-    SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
-
+    UserDetailsService userDetailsService() {
         var user = User.withUsername("bookstore")
                 .password("password")
                 .authorities("read")
                 .build();
-        http.userDetailsService(new InMemoryUserDetailsManager(user));
-
-        return http.build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
