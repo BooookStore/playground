a4859e054841e875b1e888b5ef3a139834e30a0b
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-30 09:22:18 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/environment/01-schema.sql
+++ spring-my-playground-exposed/environment/01-schema.sql
@@ -14,9 +14,22 @@ CREATE TABLE "permission"
     name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE "role"
+(
+    id   VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE "permission_operation"
 (
     permission VARCHAR(255) NOT NULL,
     operation  VARCHAR(255) NOT NULL,
     PRIMARY KEY (permission, operation)
+);
+
+CREATE TABLE "role_permission"
+(
+    role       VARCHAR(255) NOT NULL,
+    permission VARCHAR(255) NOT NULL,
+    PRIMARY KEY (role, permission)
 );
