7747072582ad3345e3cce63ecaaea8dc1bc39df7
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-30 09:41:45 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/postgres/OperationRepository.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/postgres/OperationRepository.kt
@@ -2,9 +2,19 @@ package booookstore.playground.springmyplaygroundexposed.adaptor.postgres
 
 import booookstore.playground.springmyplaygroundexposed.domain.Operation
 import booookstore.playground.springmyplaygroundexposed.domain.PermissionId
+import booookstore.playground.springmyplaygroundexposed.domain.RoleId
 import org.jetbrains.exposed.sql.select
 
 class OperationRepository {
+
+    fun findByRoleId(roleId: RoleId): List<Operation> =
+        (RoleTable
+                innerJoin RolePermissionTable
+                innerJoin PermissionTable
+                innerJoin PermissionOperationTable
+                innerJoin OperationTable)
+            .select { RoleTable.id eq roleId }
+            .map { Operation(it[OperationTable.id], it[OperationTable.name]) }
 
     fun findByPermissionId(id: PermissionId): List<Operation> =
         (PermissionTable innerJoin PermissionOperationTable innerJoin OperationTable)
