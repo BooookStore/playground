e72400835611f79aca4ccf766651eed594c16310
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-30 19:47:05 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OperationHandler.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OperationHandler.kt
@@ -5,13 +5,14 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.ServerResponse.*
 
 @Component
 class OperationHandler(private val operationUsecase: OperationUsecase) {
 
     fun findOperationByRoleId(serverRequest: ServerRequest): ServerResponse {
         val operations = operationUsecase.findOperationByRoleId(serverRequest.pathVariable("id"))
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(operations)
+        return ok().contentType(MediaType.APPLICATION_JSON).body(operations)
     }
 
 }
