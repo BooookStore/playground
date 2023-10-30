8f697d20d9849b98779fe7b0c9a8b4f61f680f7a
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-30 20:23:58 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/Router.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/Router.kt
@@ -3,6 +3,7 @@ package booookstore.playground.springmyplaygroundexposed.adaptor.rest
 import booookstore.playground.springmyplaygroundexposed.adaptor.rest.handler.OperationHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.function.RequestPredicates
 import org.springframework.web.servlet.function.RouterFunction
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.router
@@ -12,7 +13,7 @@ class Router {
 
     @Bean
     fun routerFunction(handler: OperationHandler): RouterFunction<ServerResponse> = router {
-        GET("/operation/{id}", handler::findOperationByRoleId)
+        GET("/operation", RequestPredicates.param("roleId") { true }, handler::findOperationByRoleId)
     }
 
 }
--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OperationHandler.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OperationHandler.kt
@@ -1,18 +1,17 @@
 package booookstore.playground.springmyplaygroundexposed.adaptor.rest.handler
 
 import booookstore.playground.springmyplaygroundexposed.usecase.OperationUsecase
-import org.springframework.http.MediaType
-import org.springframework.http.MediaType.*
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
-import org.springframework.web.servlet.function.ServerResponse.*
+import org.springframework.web.servlet.function.ServerResponse.ok
 
 @Component
 class OperationHandler(private val operationUsecase: OperationUsecase) {
 
     fun findOperationByRoleId(serverRequest: ServerRequest): ServerResponse {
-        val operations = operationUsecase.findOperationByRoleId(serverRequest.pathVariable("id"))
+        val operations = operationUsecase.findOperationByRoleId(serverRequest.param("roleId").get())
         return ok().contentType(APPLICATION_JSON).body(operations)
     }
 
