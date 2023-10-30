cc89c7e02a354d755dd5f734ec67572e29a2b39e
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-10-30 09:18:36 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/postgres/OperationRepository.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/postgres/OperationRepository.kt
@@ -2,7 +2,6 @@ package booookstore.playground.springmyplaygroundexposed.adaptor.postgres
 
 import booookstore.playground.springmyplaygroundexposed.domain.Operation
 import booookstore.playground.springmyplaygroundexposed.domain.PermissionId
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 
 class OperationRepository {
