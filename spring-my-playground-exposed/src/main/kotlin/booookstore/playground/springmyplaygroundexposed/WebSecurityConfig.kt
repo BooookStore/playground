059b9e0fea4e7d13284512afd289f5c1f9e93610
no_name <no_name@no_name>
2023-11-02 21:47:43 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/WebSecurityConfig.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/WebSecurityConfig.kt
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod.GET
 import org.springframework.http.HttpMethod.POST
+import org.springframework.security.authorization.AuthorizationDecision
+import org.springframework.security.authorization.AuthorizationManager
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext
+import java.util.function.Supplier
 
 @Configuration
 class WebSecurityConfig {
@@ -19,7 +24,7 @@ class WebSecurityConfig {
         http.csrf { it.disable() }
         http.authorizeHttpRequests {
             it
-                .requestMatchers(GET, "/order/**").hasAuthority("DISPLAY_ALL_ORDER")
+                .requestMatchers(GET, "/order/**").access(OrderAccessPolicy)
                 .requestMatchers(POST, "/operation").hasAuthority("CREATE_OPERATION")
                 .anyRequest().authenticated()
 
@@ -29,5 +34,22 @@ class WebSecurityConfig {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
+
+}
+
+object OrderAccessPolicy : AuthorizationManager<RequestAuthorizationContext> {
+
+    override fun check(
+        supplier: Supplier<Authentication>,
+        `object`: RequestAuthorizationContext
+    ): AuthorizationDecision = supplier.get().satisfy(
+        { isAuthenticated },
+        { hasAuthority("DISPLAY_ALL_ORDER") }
+    )
+
+    private fun Authentication.satisfy(vararg conditions: (Authentication).() -> Boolean): AuthorizationDecision =
+        conditions.map { it.invoke(this) }.all { it }.let(::AuthorizationDecision)
+
+    private fun Authentication.hasAuthority(authority: String) = authorities.map { it.authority }.contains(authority)
 
 }
