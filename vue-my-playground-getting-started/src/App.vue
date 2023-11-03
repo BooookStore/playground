d35c7fb6bc2bf18ea9850305e2543abf743eac75
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-11-03 15:20:41 +0900

    vue-my-playground-getting-started

--- vue-my-playground-getting-started/src/App.vue
+++ vue-my-playground-getting-started/src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+const orders = ref([
+  { id: "1", name: "order-1" },
+  { id: "2", name: "order-2" },
+]);
+
+function updateOrders() {
+  console.log("fetch orders");
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Vue Playground</h1>
+  <button @click.prevent="updateOrders">update orders</button>
+  <div v-for="order in orders" :id="order.id">
+    {{ order.name }}
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
