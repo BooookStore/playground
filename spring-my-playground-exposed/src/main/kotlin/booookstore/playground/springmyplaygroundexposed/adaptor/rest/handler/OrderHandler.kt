c183b2452782794ea339ca3922690eb353c134f5
no_name <no_name@no_name>
2023-11-04 22:13:16 +0900

    spring-my-playground-exposed

--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OrderHandler.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/rest/handler/OrderHandler.kt
@@ -28,23 +28,6 @@ class OrderHandler(val orderUsecase: OrderUsecase) {
         }
     }
 
-    fun createOrder(request: ServerRequest): ServerResponse {
-        data class RequestBody(val id: String, val name: String)
-
-        val (orderId, name) = request.body<RequestBody>()
-        val id = SecurityContextHolder.getContext().authentication.name.let(UUID::fromString)!!
-        val order = Order.acceptNewOrder(orderId, name, id)
-        orderUsecase.createOrder(order, id)
-        return accepted().build()
-    }
-
-    fun cancelOrder(request: ServerRequest): ServerResponse {
-        val orderId = request.pathVariable("id")
-        val cancelUserId = SecurityContextHolder.getContext().authentication.name.let(UUID::fromString)!!
-        orderUsecase.cancelOrder(orderId, cancelUserId)
-        return accepted().build()
-    }
-
     data class OrderView(val orderId: OrderId, val orderName: String, val status: OrderStatusView) {
 
         data class OrderStatusView(val status: String, val occurredOn: LocalDateTime, val userId: UserId)
@@ -63,5 +46,23 @@ class OrderHandler(val orderUsecase: OrderUsecase) {
             userId = status().userId
         )
     )
+
+
+    fun createOrder(request: ServerRequest): ServerResponse {
+        data class RequestBody(val id: String, val name: String)
+
+        val (orderId, name) = request.body<RequestBody>()
+        val id = SecurityContextHolder.getContext().authentication.name.let(UUID::fromString)!!
+        val order = Order.acceptNewOrder(orderId, name, id)
+        orderUsecase.createOrder(order, id)
+        return accepted().build()
+    }
+
+    fun cancelOrder(request: ServerRequest): ServerResponse {
+        val orderId = request.pathVariable("id")
+        val cancelUserId = SecurityContextHolder.getContext().authentication.name.let(UUID::fromString)!!
+        orderUsecase.cancelOrder(orderId, cancelUserId)
+        return accepted().build()
+    }
 
 }
