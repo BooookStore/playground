4b59c6cc4973cf0d86becf891b2a904e6cbfdbff
BooookStore <6366129+BooookStore@users.noreply.github.com>
2023-11-08 23:03:20 +0900

    spring-my-playground-exposed

---
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/adaptor/postgres/PsqlOrderDetailsViewQuery.kt
@@ -0,0 +1,40 @@
+package booookstore.playground.springmyplaygroundexposed.adaptor.postgres
+
+import arrow.core.Option
+import arrow.core.firstOrNone
+import booookstore.playground.springmyplaygroundexposed.query.OrderDetailsViewQuery
+import booookstore.playground.springmyplaygroundexposed.query.OrderDetailsView
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.select
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Transactional(readOnly = true)
+class PsqlOrderDetailsViewQuery : OrderDetailsViewQuery {
+
+    override fun searchById(orderId: String): Option<OrderDetailsView> = OrderTable
+        .select { OrderTable.id eq orderId }
+        .limit(1)
+        .map {
+            OrderDetailsView(
+                orderId = it[OrderTable.id],
+                createUser = it[OrderTable.create_user].toString(),
+                name = it[OrderTable.name],
+                statusHistory = searchOrderStatusView(orderId)
+            )
+        }
+        .firstOrNone()
+
+    private fun searchOrderStatusView(orderId: String) = OrderStatusTable
+        .select { OrderStatusTable.order eq orderId }
+        .orderBy(OrderStatusTable.datetime, SortOrder.ASC)
+        .map {
+            OrderDetailsView.OrderStatusView(
+                status = it[OrderStatusTable.status],
+                occurredOn = it[OrderStatusTable.datetime],
+                user = it[OrderStatusTable.user].toString()
+            )
+        }
+
+}
--- spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/query/OrderDetailsViewQuery.kt
+++ spring-my-playground-exposed/src/main/kotlin/booookstore/playground/springmyplaygroundexposed/query/OrderDetailsViewQuery.kt
@@ -1,40 +1,7 @@
 package booookstore.playground.springmyplaygroundexposed.query
 
 import arrow.core.Option
-import arrow.core.firstOrNone
-import booookstore.playground.springmyplaygroundexposed.adaptor.postgres.OrderStatusTable
-import booookstore.playground.springmyplaygroundexposed.adaptor.postgres.OrderTable
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.select
-import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
-@Component
-@Transactional(readOnly = true)
-class OrderDetailsViewQuery {
-
-    fun searchById(orderId: String): Option<OrderDetailsView> = OrderTable
-        .select { OrderTable.id eq orderId }
-        .limit(1)
-        .map {
-            OrderDetailsView(
-                orderId = it[OrderTable.id],
-                createUser = it[OrderTable.create_user].toString(),
-                name = it[OrderTable.name],
-                statusHistory = searchOrderStatusView(orderId)
-            )
-        }
-        .firstOrNone()
-
-    private fun searchOrderStatusView(orderId: String) = OrderStatusTable
-        .select { OrderStatusTable.order eq orderId }
-        .orderBy(OrderStatusTable.datetime, SortOrder.ASC)
-        .map {
-            OrderDetailsView.OrderStatusView(
-                status = it[OrderStatusTable.status],
-                occurredOn = it[OrderStatusTable.datetime],
-                user = it[OrderStatusTable.user].toString()
-            )
-        }
-
+interface OrderDetailsViewQuery {
+    fun searchById(orderId: String): Option<OrderDetailsView>
 }
