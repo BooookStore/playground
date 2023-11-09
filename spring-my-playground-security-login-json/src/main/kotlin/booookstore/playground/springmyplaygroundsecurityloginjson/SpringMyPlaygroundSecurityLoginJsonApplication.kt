cf3077806d4ba4fb781f51a2c8b9494d353f6fd3
no_name <no_name@no_name>
2023-11-09 19:39:28 +0900

    spring-my-playground-security-login-json

---
+++ spring-my-playground-security-login-json/src/main/kotlin/booookstore/playground/springmyplaygroundsecurityloginjson/SpringMyPlaygroundSecurityLoginJsonApplication.kt
@@ -0,0 +1,11 @@
+package booookstore.playground.springmyplaygroundsecurityloginjson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMyPlaygroundSecurityLoginJsonApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMyPlaygroundSecurityLoginJsonApplication>(*args)
+}
---
+++ spring-my-playground-security-login-json/src/test/kotlin/booookstore/playground/springmyplaygroundsecurityloginjson/SpringMyPlaygroundSecurityLoginJsonApplicationTests.kt
@@ -0,0 +1,13 @@
+package booookstore.playground.springmyplaygroundsecurityloginjson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMyPlaygroundSecurityLoginJsonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
