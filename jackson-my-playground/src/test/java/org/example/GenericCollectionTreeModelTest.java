76bedd83182fe15ab3e52a8696082856d55453fe
no_name <no_name@no_name>
2024-02-03 19:59:36 +0900

    jackson

--- jackson-my-playground/src/test/java/org/example/GenericCollectionTreeModelTest.java
+++ jackson-my-playground/src/test/java/org/example/GenericCollectionTreeModelTest.java
@@ -2,11 +2,14 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GenericCollectionTreeModelTest {
 
@@ -22,6 +25,19 @@ public class GenericCollectionTreeModelTest {
                 }
                 """, Map.class);
         assertEquals(Map.of("firstName", "book", "lastName", "store"), result);
+    }
+
+    @Test
+    void cantReadObjectToList() {
+        assertThrows(MismatchedInputException.class, () -> {
+            @SuppressWarnings("unchecked")
+            List<String> result = objectMapper.readValue("""
+                    {
+                     "firstName": "book",
+                     "lastName": "store"
+                    }
+                    """, List.class);
+        });
     }
 
     public record Person(String firstName, String lastName) {
