7ffc404f9166bea7a7afb509f371a6a5ea68cc23
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-04-13 17:54:51 +0900

    clojure

--- clojure-my-playground/test/clojure_my_playground/lib_clojure_core_complement_test.clj
+++ clojure-my-playground/test/clojure_my_playground/lib_clojure_core_complement_test.clj
@@ -3,7 +3,7 @@
   (:require [clojure.test :refer :all]))
 
 (deftest complement-test
-  (testing "example1"
-    (is (= true
-           (let [not-empty? (complement empty?)]
+  (let [not-empty? (complement empty?)]
+    (testing "example1"
+      (is (= true
              (not-empty? (list 0 1 2 3)))))))
