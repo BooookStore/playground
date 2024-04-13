dbac6e22d8e84c4e8832f2016b2936c537e5ae0e
no_name <no_name@no_name>
2024-04-13 22:29:58 +0900

    clojure

--- clojure-my-playground/test/clojure_my_playground/lib_clojure_core_complement_test.clj
+++ clojure-my-playground/test/clojure_my_playground/lib_clojure_core_complement_test.clj
@@ -2,8 +2,11 @@
   #_{:clj-kondo/ignore [:refer-all]}
   (:require [clojure.test :refer :all]))
 
-(deftest complement-test
-  (let [not-empty? (complement empty?)]
-    (testing "example1"
-      (is (= true
-             (not-empty? (list 0 1 2 3)))))))
+(with-test
+  (def not-empty? (complement empty?))
+  (is (= false
+         (not-empty? (list))))
+  (is (= true
+         (not-empty? (list 0 1 2 3))))
+  (is (= false
+         (not-empty? (list)))))
