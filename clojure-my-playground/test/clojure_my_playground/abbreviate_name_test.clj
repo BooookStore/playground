c4ba3e63920511855b747356e103ceae0f5983b8
no_name <no_name@no_name>
2024-04-20 21:21:51 +0900

    clojure

---
+++ clojure-my-playground/test/clojure_my_playground/abbreviate_name_test.clj
@@ -0,0 +1,25 @@
+(ns clojure-my-playground.abbreviate-name-test
+  #_{:clj-kondo/ignore [:refer-all]}
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]))
+
+(defn formatter
+  ([head last] (str head ". " last))
+  ([head] (str head "."))
+  ([] ""))
+
+(defn destruct [name]
+  (let [[first last] (str/split name #" ")
+        [head] first]
+    [head last]))
+
+(defn abbreviate-name [name]
+  (apply formatter (remove nil? (destruct name))))
+
+(testing "abbreviate-name"
+  (is (= "B. Store"
+         (abbreviate-name "Book Store")))
+  (is (= "S."
+         (abbreviate-name "Store")))
+  (is (= ""
+         (abbreviate-name ""))))
