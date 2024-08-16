f13ff74508b568ee65ea2ed022cce33b747bceb5
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-08-16 18:06:55 +0900

    update

---
+++ rust-my-playground-modules/src/item_box_b.rs
@@ -0,0 +1,1 @@
+pub fn item_b() {}
---
+++ rust-my-playground-modules/src/main.rs
@@ -0,0 +1,12 @@
+mod item_box_b;
+mod item_box_c;
+
+fn main() {
+    item_box_a::item_a();
+    item_box_b::item_b();
+    item_box_c::item_c();
+}
+
+mod item_box_a {
+    pub fn item_a() {}
+}
