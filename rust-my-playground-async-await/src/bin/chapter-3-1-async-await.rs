7d42ecfb90f77821bce41e165cea3dde67f33b73
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-08-19 17:49:50 +0900

    update

--- rust-my-playground-async-await/src/bin/chapter-3-1-async-await.rs
+++ rust-my-playground-async-await/src/bin/chapter-3-1-async-await.rs
@@ -22,9 +22,33 @@ fn foo_ref_expanded<'a>(x: &'a u8) -> impl Future<Output = u8> + 'a {
     async move { *x }
 }
 
+async fn blocks() {
+    let my_string = "foo".to_string();
+
+    let future_one = async {
+        info!("future_one: {my_string}");
+    };
+
+    let future_two = async {
+        info!("future_two: {my_string}");
+    };
+
+    let ((), ()) = futures::join!(future_one, future_two);
+}
+
+fn move_block() -> impl Future<Output = ()> {
+    let my_string = "foo".to_string();
+    async move {
+        info!("move_block: {my_string}");
+    }
+}
+
 fn main() {
     env_logger::init();
 
     let result = block_on(bar());
     info!("bar() result: {result}");
+
+    block_on(blocks());
+    block_on(async { move_block().await });
 }
