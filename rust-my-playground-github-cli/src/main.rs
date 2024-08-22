3c582fafdbe69968b110507f736e29d61954390d
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-08-22 22:24:59 +0900

    update

--- rust-my-playground-github-cli/src/main.rs
+++ rust-my-playground-github-cli/src/main.rs
@@ -11,13 +11,14 @@ use driver::HttpGithubDriver;
 use crate::config::{Cli, Commands::List};
 use crate::use_case::list;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = Cli::parse();
     let token = env::var("TOKEN").expect("Require auth token in env with key is TOKEN");
 
     match cli.command {
         List { org } => {
-            list::list_repository_by_organization(HttpGithubDriver::new(token), &org);
+            list::list_repository_by_organization(HttpGithubDriver::new(token), &org).await;
         }
     }
 }
