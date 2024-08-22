64fcd7031e184d18239ed9fa0ab5524f417afa09
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-08-22 21:17:45 +0900

    update

--- rust-my-playground-github-cli/src/main.rs
+++ rust-my-playground-github-cli/src/main.rs
@@ -1,17 +1,20 @@
 mod config;
+mod use_case;
+
+use std::env;
 
 use clap::Parser;
 
 use crate::config::{Cli, Commands::List};
+use crate::use_case::list;
 
 fn main() {
     let cli = Cli::parse();
+    let token = env::var("TOKEN").expect("Require auth token in env with key is TOKEN");
 
     match cli.command {
         List { org } => {
-            if org.eq("rust-lang") {
-                println!("cargo");
-            }
+            list::list_repository_by_organization(&token, &org);
         }
     }
 }
---
+++ rust-my-playground-github-cli/src/use_case/list.rs
@@ -0,0 +1,3 @@
+pub fn list_repository_by_organization(auth_token: &str, organization_name: &str) {
+    println!("cargo");
+}
