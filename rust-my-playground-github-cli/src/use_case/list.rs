d76419869dbfe0548dea08d7e0502ff4d632e7d4
no_name <no_name@no_name>
2024-08-23 17:56:31 +0900

    update

--- rust-my-playground-github-cli/src/use_case/list.rs
+++ rust-my-playground-github-cli/src/use_case/list.rs
@@ -1,5 +1,10 @@
 use crate::port::GitHubPort;
 
-pub async fn list_repository_by_organization<T: GitHubPort>(github_port: T, organization_name: &str) {
-    github_port.get_organization_repositories(organization_name).await;
+pub async fn list_repository_by_organization<T: GitHubPort>(
+    github_port: T,
+    organization_name: &str,
+) {
+    github_port
+        .get_organization_repositories(organization_name)
+        .await;
 }
