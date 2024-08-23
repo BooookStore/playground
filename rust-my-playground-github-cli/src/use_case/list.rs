9b9b908a4fd62d8b1118f6564325516a56880804
no_name <no_name@no_name>
2024-08-23 18:38:29 +0900

    update

--- rust-my-playground-github-cli/src/use_case/list.rs
+++ rust-my-playground-github-cli/src/use_case/list.rs
@@ -13,13 +13,16 @@ pub async fn list_repository_by_organization<T: GitHubPort>(
 mod tests {
     use crate::port::MockGitHubPort;
     use mockall::predicate;
+    use crate::use_case::list::list_repository_by_organization;
 
     #[tokio::test]
     async fn give_exist_organization_then_output_repositories() {
         let mut mock_github_port = MockGitHubPort::new();
         mock_github_port
             .expect_get_organization_repositories()
-            .with(predicate::eq("rust"))
+            .with(predicate::eq("rust-lang"))
             .returning(|_| "cargo".to_string());
+        
+        list_repository_by_organization(mock_github_port, "rust-lang").await;
     }
 }
