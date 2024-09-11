1c0530f7b39ed879a7a0c2ab26c4181e3a15f653
no_name <no_name@no_name>
2024-09-11 14:54:36 +0900

    update

--- rust-my-playground-github-cli/src/domain/repository.rs
+++ rust-my-playground-github-cli/src/domain/repository.rs
@@ -1,6 +1,13 @@
 use crate::domain::primitive::{ContributorName, RepositoryName};
 
+#[derive(PartialEq, Debug)]
 pub struct Repository {
     pub name: RepositoryName,
     pub contributors: Vec<ContributorName>,
+}
+
+impl Repository {
+    pub fn new(name: RepositoryName, contributors: Vec<ContributorName>) -> Self {
+        Repository { name, contributors }
+    }
 }
--- rust-my-playground-github-cli/src/use_case/repository.rs
+++ rust-my-playground-github-cli/src/use_case/repository.rs
@@ -30,12 +30,13 @@ mod tests {
     use predicate::eq;
 
     use crate::domain::primitive::{ContributorName, OrganizationName, RepositoryName};
+    use crate::domain::repository::Repository;
     use crate::port::display::MockDisplayPort;
     use crate::port::github::MockGitHubPort;
     use crate::use_case::repository::output_one_organization_repository;
 
     #[tokio::test]
-    async fn output_one_organization_repository_name() {
+    async fn output_organization_repositories_with_contributors() {
         let mut stub_github_port = MockGitHubPort::new();
         stub_github_port
             .expect_get_organization_repositories()
@@ -62,17 +63,20 @@ mod tests {
 
         let mut mock_display_port = MockDisplayPort::new();
         mock_display_port
-            .expect_print_repository_with_organization()
+            .expect_print_repositories_with_contributors()
             .with(
-                eq(String::from("rust-lang")),
+                eq(OrganizationName::from("rust-lang")),
                 eq(vec![
-                    String::from("rust"),
-                    String::from("rustlings"),
-                    String::from("cargo"),
+                    Repository::new(
+                        RepositoryName::from("rust"),
+                        vec![ContributorName::from("bob"), ContributorName::from("alice")],
+                    ),
+                    Repository::new(RepositoryName::from("rustlings"), vec![]),
+                    Repository::new(RepositoryName::from("cargo"), vec![]),
                 ]),
             )
-            .times(1)
-            .return_const(());
+            .return_const(())
+            .times(1);
 
         output_one_organization_repository(
             stub_github_port,
