4e35bb3efe3ec5abd07cbfe06e9669246c141e75
no_name <no_name@no_name>
2024-09-13 20:21:40 +0900

    update

--- rust-my-playground-vise/src/main.rs
+++ rust-my-playground-vise/src/main.rs
@@ -1,3 +1,35 @@
+use anyhow::Result;
+
 fn main() {
     println!("Hello, world!");
+}
+
+trait ServerAPort {
+    fn find_customer_by_purchase_number(number: i32) -> Result<String>;
+}
+
+struct ServerAGateway {
+    host: String,
+    port: i32,
+}
+
+impl ServerAPort for ServerAGateway {
+    fn find_customer_by_purchase_number(number: i32) -> Result<String> {
+        todo!()
+    }
+}
+
+trait ServerBPort {
+    fn find_customer_by_purchase_number(number: i32) -> Result<String>;
+}
+
+struct ServerBGateway {
+    host: String,
+    port: i32,
+}
+
+impl ServerBPort for ServerBGateway {
+    fn find_customer_by_purchase_number(number: i32) -> Result<String> {
+        todo!()
+    }
 }
