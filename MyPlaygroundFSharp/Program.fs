2a9cdf5636101eb0541d642f70cca1a577ca283e
no_name <no_name@no_name>
2024-11-23 21:15:45 +0900

    update

--- MyPlaygroundFSharp/Program.fs
+++ MyPlaygroundFSharp/Program.fs
@@ -1,2 +1,25 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+
+type PongResponse = { Pong: bool }
+
+let pongResponse () = async { return { Pong = true } }
+
+type EchoRequest = { echo: string }
+
+type EchoResponse = { echo: string }
+
+let echoResponse (echoRequest: EchoRequest) : IResult = Results.Ok { echo = echoRequest.echo }
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    let app = builder.Build()
+
+    app.MapGet("/ping", Func<Async<PongResponse>> pongResponse) |> ignore
+    app.MapPost("/echo", Func<EchoRequest, IResult> echoResponse) |> ignore
+
+    app.Run()
+
+    0
