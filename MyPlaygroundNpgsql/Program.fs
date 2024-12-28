6352cc870745692fca5a4bf37a8a741386e5dd8b
BooookStore <6366129+BooookStore@users.noreply.github.com>
2024-12-28 17:14:01 +0900

    update

---
+++ MyPlaygroundNpgsql/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open System.Data.Common
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Npgsql
+open Npgsql.FSharp
+
+[<EntryPoint>]
+let main args =
+    let connectionString =
+        Sql.host "localhost"
+        |> Sql.port 5432
+        |> Sql.database "postgres"
+        |> Sql.username "postgres"
+        |> Sql.password "postgres"
+        |> Sql.formatConnectionString
+    use dataSource = NpgsqlDataSource.Create connectionString
+
+    let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddTransient<DbConnection>(fun _ -> dataSource.OpenConnection()) |> ignore
+    let app = builder.Build()
+
+    app.MapGet("/v1/book",Func<DbConnection, IResult>(fun dbConn ->
+        Results.Ok "Extream Programming"))
+        |> ignore
+
+    app.Run()
+
+    0
