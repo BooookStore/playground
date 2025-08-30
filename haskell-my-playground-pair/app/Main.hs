c24941404b30dc4bbb08b910bb474c6092dc4852
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-08-30 14:16:44 +0900

    update

--- haskell-my-playground-pair/app/Main.hs
+++ haskell-my-playground-pair/app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
+import System.Environment (lookupEnv)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  userInformationFilePath <- lookupEnv "USER_INFORMATION_FILE"
+  case userInformationFilePath of
+    Just path -> do
+      content <- readFile path
+      putStrLn $ "content: \n" ++ content
+    Nothing -> putStrLn "ERROR: USER_INFORMATION_FILE not defined in environment variable"
