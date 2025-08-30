d9c3cce12c1716451301b2125bb58f40db359cb4
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-08-30 14:36:29 +0900

    update

--- haskell-my-playground-pair/app/Main.hs
+++ haskell-my-playground-pair/app/Main.hs
@@ -3,9 +3,10 @@ import System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
-  userInformationFilePath <- lookupEnv "USER_INFORMATION_FILE"
-  case userInformationFilePath of
-    Just path -> do
-      content <- readFile path
-      putStrLn $ "content: \n" ++ content
-    Nothing -> putStrLn "ERROR: USER_INFORMATION_FILE not defined in environment variable"
+  content <- lookupEnv "USER_INFORMATION_FILE" >>= mapM readFile
+
+  case content of
+    Just c -> putStrLn $ "user-information\n" ++ c
+    Nothing -> putStrLn "ERROR: user-information can't read"
+
+  return ()
