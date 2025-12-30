d4369be10fde15d8471ccc28a4ee8d2dec6dd3d5
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-12-30 21:31:55 +0900

    ktor-matching-service

---
+++ kotlin-matching-service/src/test/kotlin/io/booookstore/command/MatchMakeCommandTest.kt
@@ -0,0 +1,14 @@
+package io.booookstore.command
+
+import org.junit.jupiter.api.Test
+
+class MatchMakeCommandTest {
+
+    @Test
+    fun execute() {
+        // val usersGateway ...
+        // val combinationsHistoryGateway ...
+        // MatchMakeCommand() ...
+    }
+
+}
