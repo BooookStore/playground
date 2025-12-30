e22ef5858a216148d632ea1fc58037a4083c6530
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-12-30 19:27:28 +0900

    ktor-matching-service

---
+++ kotlin-matching-service/src/test/kotlin/io/booookstore/domain/PotentialCombinationsTest.kt
@@ -0,0 +1,43 @@
+package io.booookstore.domain
+
+import io.booookstore.createUser
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PotentialCombinationsTest {
+
+    @Test
+    fun matchMakeEvery() {
+        val userA = createUser()
+        val userB = createUser()
+        val users = Users.of(userA, userB)
+
+        val actual = PotentialCombinations.matchMakeEvery(users)
+
+        val expected = PotentialCombinations.of(
+            Listener(userA) to Speakers.of(userA, userB),
+            Listener(userB) to Speakers.of(userA, userB),
+        )
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun narrowDown() {
+        val userA = createUser()
+        val userB = createUser()
+
+        val actual = PotentialCombinations.of(
+            Listener(userA) to Speakers.of(userA, userB),
+            Listener(userB) to Speakers.of(userA, userB),
+        ).narrowDown(ExcludeSelf)
+
+        val expected = PotentialCombinations.of(
+            Listener(userA) to Speakers.of(userB),
+            Listener(userB) to Speakers.of(userA),
+        )
+
+        assertEquals(expected, actual)
+    }
+
+}
--- ktor-matching-service/src/main/kotlin/io/booookstore/handler/PotentialCombinationHandler.kt
+++
@@ -1,9 +0,0 @@
-package io.booookstore.handler
-
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-
-suspend fun createPotentialCombination(context: RoutingContext) {
-    // TODO
-    context.call.respondText("TODO")
-}
